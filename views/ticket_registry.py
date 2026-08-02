85e5297d9baec1bf93db6a2647b651d241e94436
six1031 <joejcjam32@gmail.com>
2026-08-02 16:12:03 +0100

    Update ticket_registry.py

--- views/ticket_registry.py
+++ views/ticket_registry.py
@@ -1,3 +1,10 @@
+from views.ticket_views import (
+    VerificationTicketView,
+    ReportsTicketView,
+    ApplicationsTicketView,
+    ContactTicketView,
+)
+
 PANEL_VIEWS = {
     "verification": VerificationTicketView,
     "reports": ReportsTicketView,
